6ab7b702d7416ccb67a8f1e177b43d8aa8329eca
poufpoufproduction <johannc@poufpoufproduction.fr>
2016-05-23 17:10:57 +0200

    fix and path activity

---
+++ activities/path/path.js
@@ -0,0 +1,152 @@
+(function($) {
+    // Activity default options
+    var defaults = {
+        name        : "path",                            // The activity name
+        label       : "Path",                            // The activity label
+        template    : "template.html",                          // Activity's html template
+        css         : "style.css",                              // Activity's css style sheet
+        lang        : "en-US",                                  // Current localization
+        exercice    : [],                                       // Exercice
+        debug       : false                                     // Debug mode
+    };
+
+    var regExp = [
+        "\\\[b\\\]([^\\\[]+)\\\[/b\\\]",            "<b>$1</b>",
+        "\\\[i\\\]([^\\\[]+)\\\[/i\\\]",            "<i>$1</i>",
+        "\\\[br\\\]",                               "<br/>",
+        "\\\[blue\\\]([^\\\[]+)\\\[/blue\\\]",      "<span style='color:blue'>$1</span>",
+        "\\\[red\\\]([^\\\[]+)\\\[/red\\\]",        "<span style='color:red'>$1</span>",
+        "\\\[strong\\\](.+)\\\[/strong\\\]",        "<div class='strong'>$1</div>"
+    ];
+
+    // private methods
+    var helpers = {
+        // @generic: Check the context
+        checkContext: function(_settings){
+            var ret         = "";
+            if (!_settings.context)         { ret = "no context is provided in the activity call."; } else
+            if (!_settings.context.onquit)  { ret = "mandatory callback onquit not available."; }
+
+            if (ret.length) {
+                ret+="\n\nUsage: $(\"target\")."+_settings.name+"({'onquit':function(_ret){}})";
+            }
+            return ret;
+        },
+        // Get the settings
+        settings: function($this, _val) { if (_val) { $this.data("settings", _val); } return $this.data("settings"); },
+        // Binding clear
+        unbind: function($this) {
+            $(document).unbind("keypress keydown");
+            $this.unbind("mouseup mousedown mousemove mouseout touchstart touchmove touchend touchleave");
+        },
+        // Quit the activity by calling the context callback
+        end: function($this) {
+            var settings = helpers.settings($this);
+            helpers.unbind($this);
+            settings.context.onquit($this,{'status':'success','score':settings.score});
+        },
+        format: function(_text) {
+            for (var j=0; j<2; j++) for (var i=0; i<regExp.length/2; i++) {
+                var vReg = new RegExp(regExp[i*2],"g");
+                _text = _text.replace(vReg,regExp[i*2+1]);
+            }
+            return _text;
+        },
+        loader: {
+            css: function($this) {
+                var settings = helpers.settings($this), cssAlreadyLoaded = false, debug = "";
+                if (settings.debug) { var tmp = new Date(); debug="?time="+tmp.getTime(); }
+
+                $("head").find("link").each(function() {
+                    if ($(this).attr("href").indexOf("activities/"+settings.name+"/"+settings.css) != -1) { cssAlreadyLoaded = true; }
+                });
+
+                if(cssAlreadyLoaded) { helpers.loader.template($this); }
+                else {
+                    $("head").append("<link>");
+                    var css = $("head").children(":last");
+                    var csspath = "activities/"+settings.name+"/"+settings.css+debug;
+
+                    css.attr({ rel:  "stylesheet", type: "text/css", href: csspath }).ready(
+                        function() { helpers.loader.template($this); });
+                }
+            },
+            template: function($this) {
+                var settings = helpers.settings($this), debug = "";
+                if (settings.debug) { var tmp = new Date(); debug="?time="+tmp.getTime(); }
+
+                // Load the template
+                var templatepath = "activities/"+settings.name+"/"+settings.template+debug;
+                $this.load( templatepath, function(response, status, xhr) { helpers.loader.build($this); });
+            },
+            build: function($this) {
+                var settings = helpers.settings($this);
+
+                // Send the onLoad callback
+                if (settings.context.onload) { settings.context.onload($this); }
+
+                $this.css("font-size", Math.floor($this.height()/12)+"px");
+
+                // Locale handling
+                $this.find("h1#label").html(settings.label);
+                if (settings.locale) { $.each(settings.locale, function(id,value) {
+                    if ($.isArray(value)) {  for (var i in value) { $this.find("#"+id).append("<p>"+value[i]+"</p>"); } }
+                    else { $this.find("#"+id).html(value); }
+                }); }
+
+                // Exercice
+                if ($.isArray(settings.exercice)) {
+                    $this.find("#exercice").html("");
+                    for (var i in settings.exercice) { $this.find("#exercice").append(
+                        "<p>"+(settings.exercice[i].length?helpers.format(settings.exercice[i]):"&nbsp;")+"</p>"); }
+                } else { $this.find("#exercice").html(helpers.format(settings.exercice)); }
+
+                if (!$this.find("#splashex").is(":visible")) { setTimeout(function() { $this[settings.name]('next'); }, 500); }
+            }
+        }
+    };
+
+    // The plugin
+    $.fn.path = function(method) {
+
+        // public methods
+        var methods = {
+            init: function(options) {
+                // The settings
+                var settings = {
+                    interactive     : false
+                };
+
+                return this.each(function() {
+                    var $this = $(this);
+                    helpers.unbind($this);
+
+                    var $settings = $.extend({}, defaults, options, settings);
+                    var checkContext = helpers.checkContext($settings);
+                    if (checkContext.length) {
+                        alert("CONTEXT ERROR:\n"+checkContext);
+                    }
+                    else {
+                        $this.removeClass();
+                        if ($settings["class"]) { $this.addClass($settings["class"]); }
+                        helpers.settings($this.addClass(defaults.name), $settings);
+                        helpers.loader.css($this);
+                    }
+                });
+            },
+            next: function() {
+                var $this = $(this) , settings = helpers.settings($this);
+                settings.interactive = true;
+            },
+            quit: function() {
+                var $this = $(this) , settings = helpers.settings($this);
+                settings.context.onquit($this,{'status':'abort'});
+            }
+        };
+
+        if (methods[method])    { return methods[method].apply(this, Array.prototype.slice.call(arguments, 1)); } 
+        else if (typeof method === 'object' || !method) { return methods.init.apply(this, arguments); }
+        else { $.error( 'Method "' +  method + '" does not exist in path plugin!'); }
+    };
+})(jQuery);
+
