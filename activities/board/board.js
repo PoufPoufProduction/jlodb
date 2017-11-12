70a621a6cef4725cb9a02ebef1e34eaf5fcfa182
JohannC <johannc@poufpoufproduction.fr>
2017-11-12 21:05:58 +0100

    new activities board

--- activities/board/board.js
+++ activities/board/board.js
@@ -8,9 +8,9 @@
         lang        : "en-US",                                  // Current localization
         paintmode   : 0,                        // O:opaque, 1:add, 2:sub, 3:def
         colors      : [[0,0,0],[255,255,255]],
-        colorsfont  : 2,
+        colorsfont  : 1.7,
         brushes     : [[3,3]],
-        brushesfont : 3,
+        brushesfont : 1.7,
         exercice    : [],                                       // Exercice
         background  : "",
         debug       : false                                     // Debug mode
@@ -108,56 +108,180 @@
                 // PREPARE MODE + COLORS + BRUSHES
                 setTimeout(function() { $this.find("#modes #mmode"+settings.paintmode).show(); }, 100);
                 
-                for (var i in settings.colors) {
+                for (var i=0; i<settings.colors.length; i++ ) {
                     var c=settings.colors[i];
                     var $elt = $("<div class='icon' id='c"+i+"' "+
-                             "style='background-color:rgb("+c[0]+","+c[1]+","+c[2]+")'></div>");
+                             "style='background-color:rgb("+c.rgb[0]+","+c.rgb[1]+","+c.rgb[2]+")'></div>");
+                    if (c.content) {
+						if (c.content.indexOf(".svg")!=-1) 	{ $elt.append("<img src='"+c.content+"' alt=''/>"); }
+						else 								{ $elt.append(c.content); }
+					}
+					
+                    $elt.bind("touchstart mousedown",function(_event) {
+						$(this).closest('.board').board('menu','color',$(this).attr("id"));
+						_event.preventDefault();
+					});
+					
+                    if (c.number) { $elt.append("<div class='number'>"+c.number+"</div>"); }
                     $this.find("#colors").append($elt);
                 }
                 $this.find("#colors").css("font-size",settings.colorsfont+"em");
+                helpers.color($this,0);
                 
                 var maxw=0,maxh=0;
-                var sizes=[];
                 for (var i in settings.brushes) {
                     var brush=settings.brushes[i];
-                    var bh=brush.length, bw = 0;
+                    var bh=brush.bitmap.length, bw = 0;
                     maxh=Math.max(maxh,bh);
-                    for (var j in brush) {
+                    for (var j in brush.bitmap) {
                         var bit = 1;
-                        for (var b=0; b<8; b++) { if ((brush[j]&bit)!=0) { bw=b+1; } bit*=2; }
+                        for (var b=0; b<8; b++) { if ((brush.bitmap[j]&bit)!=0) { bw=b+1; } bit*=2; }
                     }
                     maxw=Math.max(maxw,bw);
-                    sizes.push([bw,bh]);
+                    var bitmap=[];
+                    for (var j=0; j<bh; j++) { var line=[]; for (var i=0; i<bw; i++) { line.push(0); } bitmap.push(line); }
+                    settings.brushdata.push({size:[bw,bh], bitmap:bitmap});
                 }
                 var max=Math.max(maxw,maxh);
                 for (var i=0; i<settings.brushes.length; i++) {
                     var brush=settings.brushes[i];
-                    var $elt=$("<div class='icon' id='b'"+i+"'></div>");
-                    var svg="<svg width='100%' height='100%' viewBox='0 0 56 56'>";
-                    for (var j=0; j<brush.length; j++) {
+                    var $elt=$("<div class='icon' id='b"+i+"'></div>");
+                    var svg="<svg width='100%' height='100%' viewBox='0 0 "+(8+max*8)+" "+(8+max*8)+"'>";
+                    for (var j=0; j<brush.bitmap.length; j++) {
                         var bit = 1;
-                        var offx = (max-sizes[i][0])/2;
-                        var offy = (max-sizes[i][1])/2;
+                        var offx = (max-settings.brushdata[i].size[0])/2;
+                        var offy = (max-settings.brushdata[i].size[1])/2;
                         for (var b=0; b<max; b++) {
-                            if ((brush[j]&bit)!=0) {
-                                svg+="<rect x='"+(4+(48/max)*(max-b-1-offx))+"' y='"+(4+(48/max)*(j+offy))+"' width='"+((48/max)+0.5)+"' height='"+((48/max)+0.5)+"'/>";
+                            if ((brush.bitmap[j]&bit)!=0) {
+								settings.brushdata[i].bitmap[j][settings.brushdata[i].size[0]-b-1]=1;
+                                svg+="<rect x='"+(4+8*(max-b-1-offx))+"' y='"+(4+8*(j+offy))+"' width='8.5' height='8.5'/>";
                             }
                             bit*=2;
                         }
                     }
                     svg+="</svg>";
+                    $elt.bind("touchstart mousedown",function(_event) {
+						$(this).closest('.board').board('menu','brush',$(this).attr("id"));
+						_event.preventDefault();
+					});
                     $elt.html(svg);
+                    if (brush.number) {
+						$elt.append("<div class='number'>"+brush.number+"</div>");
+					}
                     $this.find("#brushes").append($elt);
                 }
                 $this.find("#brushes").css("font-size",settings.brushesfont+"em");
+                helpers.brush($this,0);
                 
+                // BUILD MODEL
+                if (!settings.model.colors) { settings.model.colors=$.extend({},settings.colors); }
+				else if (settings.model.colormode=="append") {
+					for (var i in settings.colors) { settings.model.colors.unshift(settings.colors[i]); }
+				}
+                max=Math.max(settings.size[0], settings.size[1]);
+                var size=(6/max)/1.2;
+                for (var j=0; j<settings.size[1]; j++) for (var i=0; i<settings.size[0]; i++) {
+					var cid=settings.model.content[i+j*settings.size[0]];
+					if (cid!='.') {
+						var c=settings.model.colors[cid];
+						var $elt = $("<div style='font-size:"+size+"em;top:"+(1.2*j)+"em;left:"+(1.2*i)+"em;"+
+										"background-color:rgb("+c.rgb[0]+","+c.rgb[1]+","+c.rgb[2]+")'></div>");
+						if (c.content) {
+							if (c.content.indexOf(".svg")!=-1) 	{ $elt.append("<img src='"+c.content+"' alt=''/>"); }
+							else 								{ $elt.append(c.content); }
+						}
+						$this.find("#model").append($elt);
+					}
+				}
+				
+				// BUILD BOARD
+				if (!settings.board.colors) { settings.board.colors=$.extend({},settings.colors); }
+				else if (settings.board.colormode=="append") {
+					for (var i in settings.colors) { settings.board.colors.unshift(settings.colors[i]); }
+				}
+				var margin=[0,0];
+				if (settings.board.margin) {
+					if ($.isArray(settings.board.margin)) { margin=settings.board.margin; }
+					else { margin=[settings.board.margin,settings.board.margin]; }
+				}
+				max=Math.max(settings.size[0]+2*margin[0], settings.size[1]+2*margin[1]);
+				$this.find("#board>div").css("font-size",(10/max)+"em");
+                for (var j=0; j<settings.size[1]; j++) for (var i=0; i<settings.size[0]; i++) {
+					var cid=settings.board.content[i+j*settings.size[0]];
+					if (cid!='.') {
+						var c=settings.board.colors[cid];
+						var $elt = $("<div class='c' id='c"+i+"x"+j+"' "+
+								     "style='top:"+(1.2*(j+margin[1]))+"em;left:"+(1.2*(i+margin[0]))+"em;"+
+										"background-color:rgb("+c.rgb[0]+","+c.rgb[1]+","+c.rgb[2]+")'></div>");
+						if (c.content) {
+							if (c.content.indexOf(".svg")!=-1) 	{ $elt.append("<img src='"+c.content+"' alt=''/>"); }
+							else 								{ $elt.append(c.content); }
+						}
+						$this.find("#board>div").append($elt);
+					}
+				}
                 
                 // Optional devmode
                 if (settings.dev) { $this.find("#devmode").show(); }
 
                 if (!$this.find("#splashex").is(":visible")) { setTimeout(function() { $this[settings.name]('next'); }, 500); }
             }
-        }
+        },
+        brush: function($this, _id) {
+			var settings = helpers.settings($this);
+			$this.find("#brushes .icon").removeClass("s");
+			if (settings.brushid==_id) { settings.brushid=-1; }
+			else {
+				$this.find("#brushes #b"+_id).addClass("s");
+				settings.brushid=_id;
+			}
+			helpers.drawbrush($this);
+		},
+        color: function($this, _id) {
+			var settings = helpers.settings($this);
+			$this.find("#colors .icon").removeClass("s");
+			if (settings.colorid==_id) { settings.colorid=-1; }
+			else {
+				$this.find("#colors #c"+_id).addClass("s");
+				settings.colorid=_id;
+			}
+		},
+		drawbrush: function($this) {
+			var settings = helpers.settings($this);
+			$this.find("#board .t").detach();
+			if (settings.brushid!=-1) {
+				var ref=settings.brushdata[settings.brushid];
+				var rotation = settings.brushpos[2];
+				var size=(rotation%2==0)?[ref.size[0],ref.size[1]]:[ref.size[1],ref.size[0]];
+				var bitmap=[];
+                for (var j=0; j<size[1]; j++) { var line=[]; for (var i=0; i<size[0]; i++) { line.push(0); } bitmap.push(line); }
+                for (var j=0; j<ref.size[1]; j++) for (var i=0; i<ref.size[0]; i++) {
+					if (ref.bitmap[j][i]) {
+						switch(rotation) {
+							case 1:	bitmap[i][size[0]-j-1]=1;break;
+							case 2: bitmap[size[1]-j-1][size[0]-i-1]=1;break;
+							case 3: bitmap[size[1]-i-1][j]=1;break;
+							default: bitmap[j][i]=1; break;
+						}
+					}
+				}
+				for (var j=0; j<size[1]; j++) for (var i=0; i<size[0]; i++) {
+					if (bitmap[j][i]) {
+						var $elt=$("<div class='t' style='top:"+j+"em;left:"+i+"em;'/></div>");
+						var b=[
+							(j==0||!bitmap[j-1][i]),
+							(i==size[0]-1||!bitmap[j][i+1]),
+							(j==size[1]-1||!bitmap[j+1][i]),
+							(i==0||!bitmap[j][i-1]) ];
+						$elt.css("border-width",(b[0]?".1em":"0")+" "+(b[1]?".1em":"0")+" "+(b[2]?".1em":"0")+" "+(b[3]?".1em":"0"));
+						$elt.css("margin",(b[0]?"-0.1em":"0")+" "+(b[1]?"-0.1em":"0")+" "+(b[2]?"-0.1em":"0")+" "+(b[3]?"-0.1em":"0"));
+						$this.find("#board>div").append($elt);
+						
+					}
+				}
+				
+			}
+		}
     };
 
     // The plugin
@@ -168,7 +292,11 @@
             init: function(options) {
                 // The settings
                 var settings = {
-                    interactive     : false
+                    interactive     : false,
+                    brushdata		: [],
+                    brushpos		: [0,0,0],
+                    brushid			: -1,
+                    colorid			: -1
                 };
 
                 return this.each(function() {
@@ -200,7 +328,8 @@
                 var $this = $(this) , settings = helpers.settings($this);
                 helpers.quit($this);
                 settings.context.onquit($this,{'status':'abort'});
-            }
+            },
+            menu: function(_type, _id) { helpers[_type]($(this), _id.substr(1)); }
         };
 
         if (methods[method])    { return methods[method].apply(this, Array.prototype.slice.call(arguments, 1)); } 
