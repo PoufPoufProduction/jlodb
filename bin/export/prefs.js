1a1b32710014e9809c788d64bb00995233e61719
JohannC <johannc@poufpoufproduction.fr>
2019-10-02 20:37:34 +0200

    userChrome available

--- bin/export/prefs.js
+++ bin/export/prefs.js
@@ -1,2 +1,3 @@
 user_pref("security.fileuri.strict_origin_policy", false);
 user_pref("dom.disable_window_open_feature.location",false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets",true);
